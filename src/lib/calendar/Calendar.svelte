34a8545dfc91ab09953d4c9c1ba7f1e80c45dbdf
unknown <shanebyrne0963@gmail.com>
2025-02-03 21:28:35 +0000

    Add date content to calendar

--- src/lib/calendar/Calendar.svelte
+++ src/lib/calendar/Calendar.svelte
@@ -1,12 +1,45 @@
 <script lang="ts">
   import CalendarHeader from "./CalendarHeader.svelte";
-  let days = new Array(42);
+  let days: number[] = [];
+
+  // Find the date of the first day of this month
+  const today = new Date();
+  const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastDay = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    0
+  ).getDate();
+
+  // Find which day the first of this month lands on, with 0 being Monday and 6 being Sunday
+  let startDate = firstDay.getDay() - 1;
+
+  if (startDate < 0) {
+    startDate += 7;
+  }
+
+  // Add
+  for (let i = 1; i <= 42; i++) {
+    if (i >= startDate && i - startDate <= lastDay) {
+      days.push(i - startDate);
+    } else {
+      days.push(0);
+    }
+  }
 </script>
 
 <CalendarHeader>
   <div id="calendar">
-    {#each days}
-      <div class="day border-1 border-indigo-100"></div>
+    {#each days as day}
+      <div
+        class="day border-1 border-indigo-200 {day === 0
+          ? 'bg-indigo-100'
+          : ''}"
+      >
+        {#if day > 0}
+          {day}
+        {/if}
+      </div>
     {/each}
   </div>
 </CalendarHeader>
@@ -20,6 +53,8 @@
 
   .day {
     width: 100%;
+    padding: 2px;
+    text-align: right;
     aspect-ratio: 1 / 1;
     box-sizing: border-box;
   }
