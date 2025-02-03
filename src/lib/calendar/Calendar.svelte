7a5c148b61e552cb31b81f7e7714e264a21d21f2
unknown <shanebyrne0963@gmail.com>
2025-02-03 21:10:44 +0000

    Add monthly calendar format day cells

---
+++ src/lib/calendar/Calendar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import CalendarHeader from "./CalendarHeader.svelte";
+  let days = new Array(42);
+</script>
+
+<CalendarHeader>
+  <div id="calendar">
+    {#each days}
+      <div class="day border-1 border-indigo-100"></div>
+    {/each}
+  </div>
+</CalendarHeader>
+
+<style>
+  #calendar {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(6, 1fr);
+  }
+
+  .day {
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/lib/calendar/CalendarHeader.svelte
@@ -0,0 +1,20 @@
+<div id="calendar-container">
+  <div id="calendar-header">
+    <div id="calendar-view-panel"></div>
+    <div id="calendar-navigation"></div>
+  </div>
+  <slot />
+</div>
+
+<style>
+  #calendar-container {
+    height: calc(100vh - 1.5em);
+    display: grid;
+    grid-template-rows: 6.6em 1fr;
+  }
+
+  #calendar-header {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+  }
+</style>
