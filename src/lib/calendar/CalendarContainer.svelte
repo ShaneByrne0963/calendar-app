dc9b1067d166a83b88df8ad7b75d5afa3c17c7ce
unknown <shanebyrne0963@gmail.com>
2025-02-04 18:38:09 +0000

    Update date navigation buttons to use Graphic component

--- src/lib/calendar/CalendarContainer.svelte
+++ src/lib/calendar/CalendarContainer.svelte
@@ -46,16 +46,14 @@
         class="cursor-pointer"
         onclick={() => handleDateDisplayChange(-1)}
       >
-        <svg
-          class="h-6 w-6 fill-current md:h-8 md:w-8"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          ><path
-            d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
-          ></path></svg
-        >
+        <Graphic
+          width="6"
+          height="6"
+          pathWidth={24}
+          pathHeight={24}
+          path={"back"}
+          extraClass="fill-current"
+        ></Graphic>
       </button>
       <div id="date-label">{months[mDisplayMonth]} {mDisplayYear}</div>
       <button
@@ -63,15 +61,14 @@
         class="cursor-pointer"
         onclick={() => handleDateDisplayChange(1)}
       >
-        <svg
-          class="h-6 w-6 fill-current md:h-8 md:w-8"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          ><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
-          ></path></svg
-        >
+        <Graphic
+          width="6"
+          height="6"
+          pathWidth={24}
+          pathHeight={24}
+          path={"forward"}
+          extraClass="fill-current"
+        ></Graphic>
       </button>
     </div>
     <div id="date-headings" class="border-1 border-blue-300 bg-blue-200">
@@ -116,10 +113,5 @@
   #date-label {
     min-width: 30%;
     text-align: center;
-  }
-
-  svg {
-    width: 3em;
-    aspect-ratio: 1 / 1;
   }
 </style>
