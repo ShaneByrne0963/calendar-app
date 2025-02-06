477b99612758f4d709091b68babbb8c2c8d2bc8d
unknown <shanebyrne0963@gmail.com>
2025-02-06 20:24:38 +0000

    Add select input component

---
+++ src/lib/inputs/Select.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  let {
+    id,
+    label,
+    value,
+    options,
+    disabled = false,
+    alignment = "y",
+  } = $props();
+
+  const onChange = (e: Event) => {
+    value.value = (e.target as HTMLInputElement).value;
+  };
+</script>
+
+<div class={alignment + " mb-4"}>
+  <label for={id}>{label}</label>
+  <select
+    {id}
+    name={id}
+    value={value.value}
+    class="select select-bordered w-full"
+    {disabled}
+    onchange={onChange}
+  >
+    {#each options as opt}
+      <option>{opt}</option>
+    {/each}
+  </select>
+</div>
+
+<style>
+  .x {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    align-items: center;
+  }
+
+  select {
+    color: black;
+  }
+</style>
--- src/lib/menu/items/add/AddActivityInputs.svelte
+++ src/lib/menu/items/add/AddActivityInputs.svelte
@@ -3,11 +3,14 @@
   import { calendarData } from "../../../../shared.svelte";
   import { dateToInputValue } from "../../../../helpers";
   import AddItem from "../AddItem.svelte";
+  import Select from "../../../inputs/Select.svelte";
 
   let { handleBack, singular } = $props();
 
   // The default value for the start date is the selected date
   let name = $state({ value: "" });
+  const occurences = ["Fixed", "Varying", "Flexible"];
+  let occurence = $state({ value: occurences[0] });
   let startDate = $state({ value: dateToInputValue(calendarData.selected) });
   let endDate = $state({ value: "" });
 
@@ -20,20 +23,29 @@
 
 <AddItem {handleBack} {singular} {createItem}>
   <Input id="item-name" label="Name" bind:value={name}></Input>
-  <Input
-    id="item-start-date"
-    type="date"
-    label="Start Date"
-    alignment="x"
-    bind:value={startDate}
-  ></Input>
-  <Input
-    id="item-end-date"
-    type="date"
-    label="End Date"
-    alignment="x"
-    bind:value={endDate}
-    required={false}
-  ></Input>
-  {name.value}
+  <Select
+    id="item-occurence"
+    label="Occurence"
+    bind:value={occurence}
+    options={occurences}
+  ></Select>
+
+  {#if occurence.value === "Fixed"}
+    <Input
+      id="item-start-date"
+      type="date"
+      label="Start Date"
+      alignment="x"
+      bind:value={startDate}
+    ></Input>
+
+    <Input
+      id="item-end-date"
+      type="date"
+      label="End Date"
+      alignment="x"
+      bind:value={endDate}
+      required={false}
+    ></Input>
+  {/if}
 </AddItem>
