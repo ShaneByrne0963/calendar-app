e4c4332f8752076daeb26d61e16926d365be66cb
unknown <shanebyrne0963@gmail.com>
2025-02-10 10:29:14 +0000

    Add ability to close info component

--- src/lib/global/Info.svelte
+++ src/lib/global/Info.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
   import Graphic from "./Graphic.svelte";
 
-  let { children } = $props();
+  let { children, closable = true } = $props();
+  let closing = $state(false);
+  let closed = $state(false);
+
+  function close() {
+    closing = true;
+  }
+
+  function ontransitionend(e: TransitionEvent) {
+    let target = e.target as HTMLElement;
+    if (target.classList.contains("info-container")) {
+      target.classList.add("closed");
+    }
+  }
 </script>
 
-<div class="info-container border-info text-info text-xs mb-3">
+<div
+  class="info-container border-info text-info text-xs mb-3 relative
+    {closing ? ' closing' : ''}{closed ? ' closed' : ''}"
+  {ontransitionend}
+>
   <Graphic width={24} height={24} path="info" fill="#00b8ff" extraClass="mr-2"
   ></Graphic>
   <p>{@render children()}</p>
+  {#if closable}
+    <button aria-label="Close window" class="close absolute" onclick={close}
+      >&times;</button
+    >
+  {/if}
 </div>
 
 <style>
@@ -18,5 +40,31 @@
     display: grid;
     grid-template-columns: auto 1fr;
     align-items: center;
+    transition: opacity 0.5s ease-in-out;
+
+    &:hover .close {
+      opacity: 0.6;
+    }
+
+    &.closing {
+      opacity: 0;
+    }
+
+    &.closed {
+      display: none;
+    }
+  }
+
+  .close {
+    color: white;
+    font-size: 1.5rem;
+    top: 0;
+    right: 8px;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+
+    &:hover {
+      opacity: 1 !important;
+    }
   }
 </style>
