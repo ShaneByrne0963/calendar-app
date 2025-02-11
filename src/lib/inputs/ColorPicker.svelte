ce0175346f9f983e9df48b89a90a89cddc4d0c9b
unknown <shanebyrne0963@gmail.com>
2025-02-11 11:29:49 +0000

    Add list of colors to color picker

--- src/lib/inputs/ColorPicker.svelte
+++ src/lib/inputs/ColorPicker.svelte
@@ -1,6 +1,58 @@
 <script lang="ts">
   import { menuData } from "../../shared.svelte";
 
+  // The main colors of DaisyUI
+  const mainColors = [
+    "secondary",
+    "accent",
+    "success",
+    "info",
+    "warning",
+    "error",
+    "white",
+  ];
+
+  // The list of Tailwind colors, divided into 2 sections
+  const colorList1 = [
+    "red",
+    "orange",
+    "amber",
+    "yellow",
+    "lime",
+    "green",
+    "emerald",
+    "teal",
+    "cyan",
+    "sky",
+    "blue",
+  ];
+  const colorList2 = [
+    "indigo",
+    "violet",
+    "purple",
+    "fuchsia",
+    "pink",
+    "rose",
+    "slate",
+    "gray",
+    "zinc",
+    "neutral",
+    "stone",
+  ];
+
+  function getColorList(colors: string[]) {
+    let arr = [];
+    const shades = [100, 200, 300, 400, 500];
+
+    for (let shd of shades) {
+      for (let col of colors) {
+        arr.push(`${col}-${shd}`);
+      }
+    }
+    return arr;
+  }
+
+  // Closes the color picker when the user clicks away
   function clickAway(e: Event) {
     let target = e.target as HTMLElement;
     if (!target.closest(".color-picker") && !target.closest(".color-input")) {
@@ -11,21 +63,65 @@
       document.removeEventListener("click", clickAway);
     }
   }
-
   setTimeout(() => document.addEventListener("click", clickAway));
 </script>
 
 <div
-  class="color-picker bg-neutral"
-  style="left: {menuData.colorPicker.x}px; top: {menuData.colorPicker.y}px"
-></div>
+  class="color-picker bg-neutral border-neutral"
+  style="left: {menuData.colorPicker.x - 32}px; top: {menuData.colorPicker.y +
+    16}px"
+>
+  <div class="row mb-3">
+    {#each mainColors as col}
+      <button aria-label="Select color" class="bg bg-{col} rounded-sm"></button>
+    {/each}
+  </div>
+  <div class="row mb-3">
+    {#each getColorList(colorList1) as col}
+      <button aria-label="Select color" class="bg bg-{col} rounded-sm"></button>
+    {/each}
+  </div>
+  <div class="row">
+    {#each getColorList(colorList2) as col}
+      <button aria-label="Select color" class="bg bg-{col} rounded-sm"></button>
+    {/each}
+  </div>
+</div>
 
 <style>
   .color-picker {
     z-index: 4;
     position: absolute;
     width: 480px;
-    height: 360px;
+    min-height: 360px;
     border-radius: 16px;
+    padding: 8px;
+
+    &::before {
+      content: "";
+      display: block;
+      border-width: 16px;
+      border-color: transparent;
+      border-bottom-color: inherit;
+      position: absolute;
+      width: 0;
+      height: 0;
+      top: -32px;
+      left: 16px;
+    }
+
+    .row {
+      display: grid;
+      grid-template-columns: repeat(11, 1fr);
+      gap: 2px;
+
+      .btn,
+      .bg {
+        width: 100%;
+        height: auto;
+        min-height: auto;
+        aspect-ratio: 1 / 1;
+      }
+    }
   }
 </style>
